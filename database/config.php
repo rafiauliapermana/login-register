3a5122b4800035aa0437bf8448cc82cb82d5bb98
rafiauliapermana <72639854+rafiauliapermana@users.noreply.github.com>
2022-09-16 22:16:31 +0700

    Create config.php

---
+++ database/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'admin';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$koneksi = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+?>
